f26424ccf4be175063a59a666df307211a1e6a92
David Pocsaji <pocsajid@gmail.com>
2026-01-23 14:59:01 +0100

    CPP-OOP

---
+++ Nasri/23_01_2026/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// fill is a range of elements with a specified value fill(beginn, end value)
+
+int main(){
+
+
+
+        const int SIZE = 99;
+        std::string foods[SIZE];
+
+            fill(foods, foods + (SIZE/3), "pizza");
+            fill(foods + (SIZE/3), foods + (SIZE/3)*2, "Hamburger");
+            fill(foods + (SIZE/3)*2, foods + SIZE, "Hotdogs");
+
+        for(std::string food : foods ){
+            std::cout << food << '\n';
+        }
+
+
+    return 0;
+}
+
